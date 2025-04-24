5882731a484fc24be4392c83e9905f945dd72cf5
LeonHatches <jhatches@unsa.edu.pe>
2025-04-24 18:13:17 -0500

    | Laboratorio 3 | Ejercicio 1 |: Plantamiento del problema y solución

---
+++ Laboratorio/Laboratorio-3/Ejercicio-1.js
@@ -0,0 +1,8 @@
+/*
+   El problema nos plantea que debemos tener una función que reciba un número
+   según el día actual del 0 al 6 con el objeto Date. Debemos devolver el dia
+   en cadena o texto.
+
+   Mi solución será crear un objeto Date, usar su método getDay() y la función
+   mediante un arreglo inicializado de los días de la semana.
+*/
