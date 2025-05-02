3a554e1511b305a11d05ded5d15e177624543e7b
LeonHatches <jhatches@unsa.edu.pe>
2025-05-02 17:11:25 -0500

    | Laboratorio 4 | Ejercicio 1 |: Estructura - Idea del Programa

---
+++ Laboratorio/Laboratorio-4/Programas/Ejercicio-1.js
@@ -0,0 +1,18 @@
+const xhttp = new XMLHttpRequest();
+
+xhttp.onload = function () {
+
+	let lista = crearLista(this);
+
+	document.getElementById("Lista").innerHTML = 
+}
+
+function crearLista (xhttp) {
+	let lista = "";
+
+/*
+	for (let i = 0 ; i < length ; i++ ) {
+	
+	}
+
+*/
