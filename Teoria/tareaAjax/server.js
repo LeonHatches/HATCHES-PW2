9307a48e4862e939938e66683e3795fe9627df16
LeonHatches <jhatches@unsa.edu.pe>
2025-05-03 15:13:21 -0500

    | Tarea-Ajax | Server |: Creación de ruta para el JS del problema 1

--- Teoria/tareaAjax/server.js
+++ Teoria/tareaAjax/server.js
@@ -11,3 +11,8 @@ app.listen(3000, () => {
 app.get('/problema1.html', (request, response) => {
 	response.sendFile(path.resolve(__dirname, 'problema1.html'))
 });
+
+app.get('/data.json', (request, response) => {
+	const data = require('data.json');
+  response.json(data);
+});
