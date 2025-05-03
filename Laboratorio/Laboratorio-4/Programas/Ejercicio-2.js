0222cc37f463f3cc9bea61157d13f2d5eff22696
LeonHatches <jhatches@unsa.edu.pe>
2025-05-03 00:22:32 -0500

    | Laboratorio 4 | Ejercicio 2 |: Primer intento del programa - Error: Lee objetos como Object

--- Laboratorio/Laboratorio-4/Programas/Ejercicio-2.js
+++ Laboratorio/Laboratorio-4/Programas/Ejercicio-2.js
@@ -11,9 +11,15 @@ xhttp.open("GET", "data.JSON");
 xhttp.send();
 
 function mostrarConfirmados (xhttp) {
+	
+	let lista = "<ul>";
+	const pais = JSON.parse(xhttp.responseText);
 
-	for (let i = 0 ; i < length ; i++ ) {
-
+	for (let i = 0 ; i < pais.length ; i++ ) {
+		lista += "<li>" + pais[i].region + ": " + pais[i].confirmed[pais[i].confirmed.length - 1] + "</li>";
 	}
 
+	lista += "</ul>";
+
+	return lista;
 }
