28430bbb1dd0d2e8d4c6056b2a9efb9a9d0bb88d
leonhatches <jhatches@unsa.edu.pe>
2025-05-03 21:51:17 -0500

    | Tarea-Ajax | Server |: Se agrega un index dinámico

--- Teoria/tareaAjax/server.js
+++ Teoria/tareaAjax/server.js
@@ -8,8 +8,19 @@ app.listen(3000, () => {
 	console.log("Escuchando en: http://localhost:3000");
 });
 
-app.get('/problema1.html', (request, response) => {
-	response.sendFile(path.resolve(__dirname, 'problema1.html'))
+app.get('/', (request, response) => {
+  const index = `
+    <!DOCTYPE html>
+    <html>
+    	<body>
+      	<h1>Lista de Problemas Resueltos</h1>
+      	<a href="http://localhost:3000/problema1.html">Problema 1</a>
+  			<a href="http://localhost:3000/problema2.html">Problema 2</a>
+    	</body>
+    </html>
+  `;
+
+  response.send(index);
 });
 
 app.get('/data.json', (request, response) => {
