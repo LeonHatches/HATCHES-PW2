f045a85b122e3e024930e86fcfb7a42ab1bd8822
LeonHatches <jhatches@unsa.edu.pe>
2025-05-06 23:25:26 -0500

    | Laboratorio 4 | Indentación | JS |: Correción de indentación

--- Laboratorio/Laboratorio-4/Programas/Ejercicio-8.js
+++ Laboratorio/Laboratorio-4/Programas/Ejercicio-8.js
@@ -20,7 +20,7 @@ function mostrarGrafico (data) {
 
 		var options = {
 			hAxis: { title: "Días" },
-      vAxis: { title: "Confirmados" },
+			vAxis: { title: "Confirmados" },
 			"title": "Gráfico Comparativo entre Regiones por Día del Perú",
 			"width": 4000,
 			"height": 2000
@@ -37,12 +37,15 @@ function agregarDatos (grafico, data) {
 	let dato1, dato2;
 
 	for (let i = 0 ; i < data.length ; i++) {
-		if (data[i].region != "Callao" && data[i].region != "Lima")
+		
+		if (data[i].region != "Callao" && data[i].region != "Lima") {
 			grafico.addColumn("number", data[i].region);
+		}
 	}
 	
 	// Agrega las fechas
 	for (let i = 0 ; i < 1 ; i++) {
+		
 		for (let j = 0 ; j < data[i].confirmed.length - 1 ; j++) {
 			Rows.push( [`${j + 1}`] );
 		}
@@ -50,13 +53,16 @@ function agregarDatos (grafico, data) {
 
 	// Agrega los datos de contagios
 	for (let i = 0 ; i < data.length ; i++) {
-		if (data[i].region != "Callao" && data[i].region != "Lima")
+		
+		if (data[i].region != "Callao" && data[i].region != "Lima") {
+			
 			for (let j = 0 ; j < data[i].confirmed.length - 1 ; j++) {
 				
 				dato1 = parseInt(data[i].confirmed[j].value);
 				dato2 = parseInt(data[i].confirmed[j+1].value);
 
 				Rows[j].push(dato2 - dato1);
+			}
 		}
 	}
 
