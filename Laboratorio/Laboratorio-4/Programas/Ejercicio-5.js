56760232973ccd392b78aa721783113d85b46713
leonhatches <jhatches@unsa.edu.pe>
2025-05-06 21:15:45 -0500

    | Laboratorio 4 | Ejercicio 5 |: Se crean iteraciones para colocar fechas y poner los datos de contagios

--- Laboratorio/Laboratorio-4/Programas/Ejercicio-5.js
+++ Laboratorio/Laboratorio-4/Programas/Ejercicio-5.js
@@ -22,13 +22,23 @@ function mostrarGrafico (data) {
 
 function agregarDatos (data) {
 	
-	const Rows = [];
+	const Row = [];
 	
 	for (let i = 0 ; i < data.length ; i++) {
 		grafico.addColumn("number", data[i].region);
 	}
 	
-	for (let j = 0 ; j < data[i].confirmed.length ; j++) {
-		Rows.push();
+	// Agrega las fechas
+	for (let i = 0 ; i < 1 ; i++) {
+		for (let j = 0 ; j < data[i].confirmed.length ; j++) {
+			Row.push( [data[i].confirmed[j].date] );
+		}
+	}
+
+	// Agrega los datos de contagios
+	for (let i = 0 ; i < data.length ; i++) {
+		for (let j = 0 ; j < data[i].confirmed.length ; j++) {
+			Row[j].push( parseInt(data[i].confirmed[j].value) );
+		}
 	}
 }
