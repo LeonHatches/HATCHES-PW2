4ea815cc89a64742b9e8293fbad3f983f867d39e
leonhatches <jhatches@unsa.edu.pe>
2025-05-13 17:02:17 -0500

    | Laboratorio 5 | Correción | : Se utiliza un mejor método

--- Laboratorio/Laboratorio-5/Ejercicio2f.py
+++ Laboratorio/Laboratorio-5/Ejercicio2f.py
@@ -11,13 +11,7 @@ for i in range(7):
         fila = fila.join(square)
 
 # Fila que comienza con cuadrado negativo
-filaNegativa = square.negative()
-
-for i in range(7):
-    if i % 2 == 0:
-        filaNegativa = filaNegativa.join(square)
-    else:
-        filaNegativa = filaNegativa.join(square.negative())
+filaNegativa = fila.negative()
 
 # Union de cada fila
 tabla = filaNegativa.up(fila)
