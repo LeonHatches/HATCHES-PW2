ff728556fc38a9dd5c36475521f55e80d9a7e755
leonhatches <jhatches@unsa.edu.pe>
2025-05-13 02:50:54 -0500

    | Laboratorio 5 | Ejercicio 2 | A | Correción | : Se corrije la salida ahora de los métodos a Picture

--- Laboratorio/Laboratorio-5/Ejercicio2a.py
+++ Laboratorio/Laboratorio-5/Ejercicio2a.py
@@ -3,10 +3,10 @@ from chessPictures import *
 
 # Se crea un Fig, donde se coloca al caballo al costado a otra
 # Fig de un caballo de color invertido
-blancoYnegro = Picture(knight.join(Picture(knight.negative())))
+blancoYnegro = knight.join(knight.negative())
 
 # Se crea una Fig, donde se crea una Fig de caballo invertido y
 # se inseta un caballo a su costado
-negroYblanco = Picture(Picture(knight.negative()).join(knight))
+negroYblanco = knight.negative().join(knight)
 
 draw(negroYblanco.up(blancoYnegro))
