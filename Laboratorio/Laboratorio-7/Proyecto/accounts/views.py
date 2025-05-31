db68656e943f535483c937420ff3ac3dc9d42109
leonhatches <jhatches@unsa.edu.pe>
2025-05-31 12:32:00 -0500

    | accounts | : Se crea un mensaje para mostrar cuando no se crea el usuario por el email, password o username

--- Laboratorio/Laboratorio-7/Proyecto/accounts/views.py
+++ Laboratorio/Laboratorio-7/Proyecto/accounts/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.models import User, auth
+from django.contrib import messages
 
 # Create your views here.
 def register(request):
@@ -13,18 +14,21 @@ def register(request):
 
         if password1 == password2:
             if User.objects.filter(username=username).exists():
-                print('Username taken')
-
+                messages.info(request, 'Username taken')
+                return redirect("register")
+                      
             elif User.objects.filter(email=email).exists():
-                print('Email taken')
+                messages.info(request, 'Email taken')
+                return redirect("register")
             
             else:
                 user = User.objects.create(username=username, password=password1, email=email, first_name=first_name, last_name=last_name)
                 user.save()
-                print('User Created')
+                messages.info(request, 'User Created')
         
         else:
-            print('Password not matching')
+            messages.info(request, 'Password not matching')
+            return redirect("register")
 
         return redirect('/')
     
