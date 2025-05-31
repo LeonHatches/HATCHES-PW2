0810577370fa911337e018816d6bdea13ca1a490
leonhatches <jhatches@unsa.edu.pe>
2025-05-31 14:08:45 -0500

    | accounts | logout | : Se crea la lógica para salir de la sesión

--- Laboratorio/Laboratorio-7/Proyecto/accounts/views.py
+++ Laboratorio/Laboratorio-7/Proyecto/accounts/views.py
@@ -54,3 +54,9 @@ def login(request):
             return redirect('login')
     else:
         return render(request, 'login.html')
+    
+
+
+def logout(request):
+    auth.logout(request)
+    return redirect('/')
